61317cd4d5eeec38f849186aee4c005e7db7c7d8
Aanjalisingh24 <aanjali23@navgurukul.org>
2025-10-30 18:11:27 +0530

    Update live link to the code

--- campus-server/server.js
+++ campus-server/server.js
@@ -7,10 +7,17 @@ import courseRoutes from "./routes/courseRoutes.js"
 import studentRoutes from "./routes/studentRoutes.js";
 import cors from "cors";
 
+
+// import cors from "cors";
+app.use(cors({
+  origin: "campus-hub-gamma.vercel.app", // your frontend URL
+  credentials: true,
+}));
+
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 
